9cc59d4317063743d3a238106022f85d242de23b
Stef van Esch <stef@marshmallow.dev>
2021-01-20 12:00:03 +0100

    Add a makeable method

--- src/Price.php
+++ src/Price.php
@@ -5,9 +5,47 @@ namespace Marshmallow\Priceable;
 use Money\Money;
 use Money\Currency;
 use Laravel\Cashier\Cashier;
+use Marshmallow\Priceable\Models\VatRate;
+use Marshmallow\Priceable\Models\Currency as PricableCurrency;
 
 class Price
 {
+    public $vatrate;
+
+    public $currency;
+
+    public $display_amount;
+
+    public $price_excluding_vat;
+
+    public $price_including_vat;
+
+    public $vat_amount;
+
+    public function make(VatRate $vatrate, PricableCurrency $currency, int $display_amount, bool $display_is_including_vat)
+    {
+        $this->vatrate = $vatrate;
+        $this->currency = $currency;
+        $this->display_amount = $display_amount;
+
+        if ($display_is_including_vat) {
+
+            /**
+             * The added price is including the VAT. We need to calculate
+             * the price without the VAT.
+             */
+            $price_excluding_vat = round(($display_amount / (100 + $vatrate->rate)) * 100);
+        } else {
+            $price_excluding_vat = $display_amount;
+        }
+
+        $this->price_excluding_vat = $price_excluding_vat;
+        $this->price_including_vat = round($price_excluding_vat * $vatrate->multiplier());
+        $this->vat_amount = $this->price_including_vat - $this->price_excluding_vat;
+
+        return $this;
+    }
+
     public function formatAmount($amount, $currency = null)
     {
         return Cashier::formatAmount($amount, $currency);
